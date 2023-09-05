81c330c2a91ae0989071dfb5cfbd1d171c928aaf
brenoasantos <brenoaas2@gmail.com>
2023-09-05 00:54:43 -0300

    register page and corrections

--- src/modules/public/Login/Login.tsx
+++ src/modules/public/Login/Login.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import { Flex, Box, Heading } from '@chakra-ui/react';
+
+import { ClickableText } from '@/components/ClickableText/ClickableText';
+import { LoginButton } from '@/components/LoginButton';
+import { ReturnButton } from '@/components/ReturnButton';
+import { TextInput } from '@/components/TextInput';
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <>
+      <Flex marginTop="3.75rem">
+        <Box>
+          <ReturnButton />
+        </Box>
+        <Flex
+          w="100%"
+          h="100hv"
+          alignItems="center"
+          justifyContent="center"
+          flexDir="column"
+          color="black"
+          paddingRight="5rem"
+        >
+          <Heading fontSize="xl">Login</Heading>
+        </Flex>
+      </Flex>
+      <Flex
+        marginTop="9rem"
+        flexDir="column"
+        justifyContent="space-between"
+        gap="2rem"
+        alignItems="left"
+        marginLeft="2rem"
+      >
+        <TextInput label="E-mail" placeholder="Digite seu e-mail" borderRadius="md" width="22rem" />
+
+        <TextInput label="Senha" placeholder="Digite a sua senha" borderRadius="md" width="22rem" />
+
+        <LoginButton label="Entrar" borderRadius="md" width="5rem" marginLeft="8.5rem" />
+
+        <ClickableText text="Ainda não está cadastrado? Cadastre-se aqui" href="/futes" />
+      </Flex>
+    </>
+  );
 };
--- src/modules/public/Register/Register.tsx
+++ src/modules/public/Register/Register.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 
 import { GreenTextBox } from '@/components/GreenTextBox';
+import { ReturnButton } from '@/components/ReturnButton';
 
 export const Register = () => {
   return (
@@ -11,12 +12,17 @@ export const Register = () => {
       gap="2rem"
       alignItems="center"
       paddingTop="3.75rem"
+      w="100%"
     >
-      <Box marginBottom="8rem">
-        <Text as="b" fontSize="lg">
-          Cadastro de Pelada
-        </Text>
-      </Box>
+      <Flex w="100%" justifyContent="space-between" marginBottom="8rem" alignItems="center">
+        <ReturnButton />
+        <Box>
+          <Text as="b" fontSize="lg">
+            Cadastro de Pelada
+          </Text>
+        </Box>
+        <Box width="4.89em"></Box>
+      </Flex>
       <Flex flexDir="column" justifyContent="space-between" gap="2rem" alignItems="center">
         <Box>
           <GreenTextBox type="text" label="E-mail" />
