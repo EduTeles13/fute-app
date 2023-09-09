7227a9dcc7380e3917cf97a93c6397a45f7be9a1
lucanevess87 <lucatets@hotmail.com>
2023-09-09 18:33:19 -0300

    auth fix

--- src/modules/public/Login/Login.tsx
+++ src/modules/public/Login/Login.tsx
@@ -1,11 +1,37 @@
-import { Flex, Box, Heading } from '@chakra-ui/react';
+import { Flex, Box, Heading, useToast } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { getSession, signIn } from 'next-auth/react';
 
 import { ClickableText } from '@/components/ClickableText/ClickableText';
 import { LoginButton } from '@/components/LoginButton';
 import { ReturnButton } from '@/components/ReturnButton';
 import { TextInput } from '@/components/TextInput';
+import { cookies } from '@/utils';
 
 export const Login = () => {
+  const { push } = useRouter();
+  const toast = useToast();
+
+  const handleLogin = async () => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: 'teste@gmail.com',
+      password: '12345678',
+    });
+    if (result?.status == 200) {
+      const session: any = await getSession();
+      cookies.setAccess(session?.user.access_token);
+      await push('/admin');
+    } else {
+      toast({
+        title: 'Usuário ou senha incorreta.',
+        duration: 3000,
+        isClosable: true,
+        status: 'error',
+      });
+    }
+  };
+
   return (
     <>
       <Flex marginTop="3.75rem">
@@ -36,7 +62,13 @@ export const Login = () => {
 
         <TextInput label="Senha" placeholder="Digite a sua senha" borderRadius="md" width="22rem" />
 
-        <LoginButton label="Entrar" borderRadius="md" width="5rem" marginLeft="8.5rem" />
+        <LoginButton
+          label="Entrar"
+          borderRadius="md"
+          width="5rem"
+          marginLeft="8.5rem"
+          onClick={handleLogin}
+        />
 
         <ClickableText text="Ainda não está cadastrado? Cadastre-se aqui" href="/futes" />
       </Flex>
