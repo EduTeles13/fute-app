6d6852945e715bb8c2857dea5ccce02c8e8af8f7
lucanevess87 <lucatets@hotmail.com>
2023-09-09 22:12:38 -0300

    fix(login): auth

--- src/modules/public/Login/Login.tsx
+++ src/modules/public/Login/Login.tsx
@@ -1,20 +1,45 @@
-import { Flex, Box, Heading } from '@chakra-ui/react';
+import { Flex, Box, Heading, useToast } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { getSession, signIn } from 'next-auth/react';
 
 import { ClickableText } from '@/components/ClickableText/ClickableText';
 import { LoginButton } from '@/components/LoginButton';
 import { ReturnButton } from '@/components/ReturnButton';
 import { TextInput } from '@/components/TextInput';
+import { cookies } from '@/utils';
 
 export const Login = () => {
+  const { push } = useRouter();
+  const toast = useToast();
+
+  const handleLogin = async () => {
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: 'teste@gmail.com',
+      password: '12345678',
+    });
+    if (result?.status == 200) {
+      const session: any = await getSession();
+      cookies.setAccess(session?.user.access_token);
+      await push('/admin');
+    } else {
+      toast({
+        title: 'Usuário ou senha incorreta.',
+        duration: 3000,
+        isClosable: true,
+        status: 'error',
+      });
+    }
+  };
+
   return (
-    <>
+    <Flex flexDir="column">
       <Flex marginTop="3.75rem">
         <Box>
           <ReturnButton />
         </Box>
         <Flex
           w="100%"
-          h="100hv"
           alignItems="center"
           justifyContent="center"
           flexDir="column"
@@ -24,22 +49,21 @@ export const Login = () => {
           <Heading fontSize="xl">Login</Heading>
         </Flex>
       </Flex>
-      <Flex
-        marginTop="9rem"
-        flexDir="column"
-        justifyContent="space-between"
-        gap="2rem"
-        alignItems="left"
-        marginLeft="2rem"
-      >
-        <TextInput label="E-mail" placeholder="Digite seu e-mail" borderRadius="md" width="21rem" />
+      <Flex flexDir="column" w="100%" justifyContent="center">
+        <TextInput label="E-mail" placeholder="Digite seu e-mail" borderRadius="md" />
 
-        <TextInput label="Senha" placeholder="Digite a sua senha" borderRadius="md" width="21rem" />
+        <TextInput label="Senha" placeholder="Digite a sua senha" borderRadius="md" />
 
-        <LoginButton label="Entrar" borderRadius="md" width="5rem" marginLeft="7.75rem" />
+        <LoginButton
+          label="Entrar"
+          borderRadius="md"
+          width="5rem"
+          marginLeft="7.75rem"
+          onClick={handleLogin}
+        />
 
         <ClickableText text="Ainda não está cadastrado? Cadastre-se aqui" href="/futes" />
       </Flex>
-    </>
+    </Flex>
   );
 };
