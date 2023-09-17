a66c75f5d9f62476b608558c5a439bd9ed02f6bf
EduTeles13 <eduardodiasteles13@gmail.com>
2023-09-17 09:55:43 -0300

    adicao de tela encontrar pelada

--- src/modules/public/Futes/Futes.tsx
+++ src/modules/public/Futes/Futes.tsx
@@ -1,5 +1,29 @@
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Flex,
+  GridItem,
+} from '@chakra-ui/react';
 import React from 'react';
 
+import { ReturnButton } from '@/components/ReturnButton';
+
 export const Futes = () => {
-  return <div>Futes</div>;
+  return (
+    <Flex marginTop="3rem">
+      <GridItem colSpan={1}>
+        <ReturnButton />
+      </GridItem>
+      <NumberInput w="70%">
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+    </Flex>
+  );
 };
